5e1c6f5e27e957911dcb418f18993ae1ddbb967f
ivy <ivyvodesign@gmail.com>
2020-07-03 11:13:18 +0700

    first commit ivy

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Col, Button} from 'react-bootstrap'
+
+
+
+export default function Jumbotron() {
+    return (
+        <div>
+             <div className="jumbotron" style={{
+            backgroundImage:`url("https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=face&w=1600&h=838&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2020%2F04%2F08%2Fairbnb-virtual-experiences-BNBV0420.jpg")`, 
+            backgroundImageWidth: '100vw',
+        }} >
+
+            <Col lg={4}><h1>Online Experiences</h1></Col>
+            <Col lg={4}><h4>Unique activities to do from home, including cooking experiences with world-renowned chefs</h4></Col>
+        </div>
+            
+
+        <div className="tags">
+            <Col>
+            <Button className="btn" variant="light">Date</Button>
+            <Button variant="light">Time of The Day</Button>
+            <Button variant="light">Price</Button>
+            <Button variant="light">Language Offered</Button>
+            </Col>
+            
+            <Col>
+            <Button variant="light">Great For Groups +</Button>
+            <Button variant="light">Family Friendly +</Button>
+            <Button variant="light">Animals +</Button>
+            <Button variant="light">Arts & Writing +</Button>
+            <Button variant="light">Cooking +</Button>
+            <Button variant="light">Dancing +</Button>
+            </Col>
+        </div>
+
+        </div>
+       
+            
+        
+    )
+}
+
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, Button, Form, FormControl, Navbar, Col } from 'react-bootstrap'
+
+
+export default function Navigation() {
+    return (
+        <div>
+            <>
+
+                <Navbar className="nav1" style={{display:"flex","justifyContent":"space-between"}}>
+
+                    <Navbar.Brand href="#home"><img style={{marginLeft:"30px"}} alt="logo" width="130" src="https://japanesquest.com/wp-content/uploads/2017/10/airbnb-logo.png" /></Navbar.Brand>
+
+                    <Nav className="nav">
+                        <Nav.Link href="#home">Host Your Home</Nav.Link>
+                        <Nav.Link href="#features">Host An Experience</Nav.Link>
+                        <Nav.Link href="#features">Help</Nav.Link>
+                        <Nav.Link href="#features">Log In</Nav.Link>
+                        <Nav.Link href="#features">Sign Up</Nav.Link>
+
+                    </Nav>
+
+
+
+                </Navbar>
+            </>
+        </div>
+    )
+}
